195a2c71708279127abc2b3b29f27acc2f35a04a
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2022-07-31 22:04:28 +0300

    1488

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,62 +7,60 @@ const daysRef = document.querySelector('span[data-days]');
 const hoursRef = document.querySelector('span[data-hours]');
 const minutesRef = document.querySelector('span[data-minutes]');
 const secondsRef = document.querySelector('span[data-seconds]');
-// console.log(daysRef);
 
 startTimerRef.disabled = true;
-const fp = flatpickr('#datetime-picker', {
+
+const flatpickrInstance = flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0].getTime());
     if (selectedDates[0] < Date.now()) {
       Notify.failure('Please choose a date in the future');
-      // window.alert('Please choose a date in the future');
+
       return;
     }
     startTimerRef.disabled = false;
-    startTimerRef.addEventListener('click', timerTick);
+    startTimerRef.addEventListener('click', countDown);
   },
 });
-function timerTick(e) {
+
+function countDown(e) {
   startTimerRef.disabled = true;
-  const targetTime = String(fp.selectedDates[0].getTime());
-  console.log(targetTime);
+  const targetTime = String(flatpickrInstance.selectedDates[0].getTime());
+
   const interIdn = setInterval(() => {
     const now = Date.now();
     const remainingTime = targetTime - now;
-    console.log(remainingTime);
+
     if (remainingTime === 0 || remainingTime < 0) {
       clearInterval(interIdn);
       Notify.failure('Time is up');
       return;
     }
-    console.log(remainingTime);
 
-    console.log(convertMs(remainingTime));
     const time = convertMs(remainingTime);
-    Rasmetka(time);
+    drawInterface(time);
   }, 1000);
 }
+
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
+
   const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
+
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
+
   const seconds = addLeadingZero(
     Math.floor((((ms % day) % hour) % minute) / second)
   );
@@ -70,7 +68,7 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-function Rasmetka(time) {
+function drawInterface(time) {
   daysRef.textContent = time.days;
   hoursRef.textContent = time.hours;
   minutesRef.textContent = time.minutes;
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,40 +1,41 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function createPromise(position, delay, step) {
+function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    for (let i = 1; i <= position; i++) {
-      position = i;
-      setTimeout(() => {
-        if (shouldResolve) {
-          resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        } else {
-          reject(`❌ Rejected promise ${position} in ${delay}ms`);
-        }
-      }, delay);
-      delay = delay + step;
-    }
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
   });
 }
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
 
-const formRef = document.querySelector('.form');
-const delayRef = document.querySelector('[name="delay"]');
-const stepRef = document.querySelector('[name="step"]');
-const amountRef = document.querySelector('[name="amount"]');
-
-formRef.addEventListener('submit', onSubmitForm);
+refs.form.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(e) {
   e.preventDefault();
 
-  let delay = Number(delayRef.value);
-  const step = Number(stepRef.value);
-  let position = Number(amountRef.value);
-  console.log('gggggggggggg');
+  let delay = Number(refs.delay.value);
+  const step = Number(refs.step.value);
+  const amount = Number(refs.amount.value);
+  let position;
 
-  createPromise(position, delay, step)
-    .then(result => {
-      Notify.success(result);
-    })
-    .catch(error => Notify.failure(error));
+  for (let i = 1; i <= amount; i++) {
+    position = i;
+    createPromise(position, delay)
+      .then(result => {
+        Notify.success(result);
+      })
+      .catch(error => Notify.failure(error));
+    delay = delay + step;
+  }
 }
