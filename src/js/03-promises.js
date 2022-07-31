d586980504289da09f49cd1179d7cd2f0f2a0914
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2022-07-31 20:07:26 +0300

    469

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,32 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const refs = {
+  formRef: document.querySelector('.form'),
+  delayRef: document.querySelector('[name="delay"]'),
+  stepRef: document.querySelector('[name="step"]'),
+  amountRef: document.querySelector('[name="amount"]'),
+};
+
+refs.formRef.addEventListener('submit', onSubmitForm);
+// const formRef = document.querySelector('.form');
+// const delayRef = document.querySelector('[name="delay"]');
+// const stepRef = document.querySelector('[name="step"]');
+// const amountRef = document.querySelector('[name="amount"]');
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let delay = Number(delayRef.value);
+  const step = Number(stepRef.value);
+  const position = Number(amountRef.value);
+
+  createPromise(position, delay)
+    .then(result => {
+      Notify.success(result);
+    })
+    .catch(error => Notify.failure(error));
+}
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -16,107 +43,3 @@ function createPromise(position, delay) {
     }
   });
 }
-
-const formRef = document.querySelector('.form');
-const delayRef = document.querySelector('[name="delay"]');
-const stepRef = document.querySelector('[name="step"]');
-const amountRef = document.querySelector('[name="amount"]');
-
-formRef.addEventListener('submit', f);
-
-function f(e) {
-  e.preventDefault();
-
-  let delay = Number(delayRef.value);
-  const step = Number(stepRef.value);
-  const amount = Number(amountRef.value);
-  let newDelay = delay + step;
-  let position = 0;
-  // let counter = 1;
-
-  createPromise(position, delay)
-    .then(result => {
-      Notify.success(result);
-    })
-    .catch(error => Notify.failure(error));
-  // counter += 1;
-
-  // for (let i = 2; i <= amount; i++) {
-  //   position = i;
-  //   createPromise(position, newDelay)
-  //     .then(result => {
-  //       Notify.success(result);
-  //     })
-  //     .catch(error => Notify.failure(error));
-  //   newDelay = newDelay + step;
-  // }
-}
-
-// let newDelay =
-
-//     setTimeout(() => {
-//       // if (counter === amount - 1) {
-//       //   return;
-//       // }
-//       createPromise(position, newDelay)
-//         .then(result => {
-//           Notify.success(result);
-//         })
-//         .catch(error => Notify.failure(error));
-//       counter = counter + 1;
-//       position = position + 1;
-//       newDelay = newDelay + step;
-//     }, newDelay);
-// }
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-
-//   const promise = new Promise((resolve, reject) => {
-//     if (shouldResolve) {
-//       resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//     }
-//     reject(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-//   return promise;
-// }
-// const formRef = document.querySelector('.form');
-// const delayRef = document.querySelector('[name="delay"]');
-// const stepRef = document.querySelector('[name="step"]');
-// const amountRef = document.querySelector('[name="amount"]');
-
-// let amount = amountRef.value;
-// // console.log(amount);
-// const currentAmount = null;
-// formRef.addEventListener('submit', constructor);
-// function constructor() {
-//   const firsDal = delayRef.value;
-//   const delay = stepRef.value;
-//   const position = 1;
-//   setTimeout(() => {
-//     // if (currentAmount === amount) {
-//     //   return;
-//     // }
-//     createPromise(position, firsDal)
-//       .then(() => {
-//         console.log(gg);
-//       })
-//       .catch(() => {
-//         console.log(ff);
-//       });
-//     currentAmount += 1;
-//     position += 1;
-//   }, firsDal);
-//   setInterval(() => {
-//     // if (currentAmount === amount) {
-//     //   return;
-//     // }
-//     createPromise(position, delStep)
-//       .then(() => {
-//         console.log(gg);
-//       })
-//       .catch(() => {
-//         console.log(ff);
-//       });
-//   }, delStep);
-// }
